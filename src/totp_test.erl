8b548f5b086195eade5ea3115be0f11c8210bdb0
Nicolas Martyanoff <khaelin@gmail.com>
2019-08-14 09:39:58 +0200

    add copyright headers to source files

--- src/totp_test.erl
+++ src/totp_test.erl
@@ -1,3 +1,16 @@
+%% Copyright 2019, JobTeaser
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
 
 -module(totp_test).
 
@@ -19,4 +32,3 @@ generate_test() ->
   ?assertEqual(89005924, Generate("2009-02-13T23:31:30Z")),
   ?assertEqual(69279037, Generate("2033-05-18T03:33:20Z")),
   ?assertEqual(65353130, Generate("2603-10-11T11:33:20Z")).
-
